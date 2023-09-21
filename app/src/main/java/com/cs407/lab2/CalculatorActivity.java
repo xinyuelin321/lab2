55e4acaa3e130dc61545fbf76825f66c357e579a
admin <1299071633@qq.com>
2023-09-21 14:16:51 -0500

    change the helloworld text

---
+++ app/src/main/java/com/cs407/lab2/CalculatorActivity.java
@@ -0,0 +1,21 @@
+package com.cs407.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator_activity);
+        textView=(TextView) findViewById(R.id.textView2);
+        Intent intent=getIntent();
+        String str=intent.getStringExtra("message");
+        textView.setText("Hello"+str);
+    }
+}
